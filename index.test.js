cf9c0b1d67b7ade69a6c6a4c4636a27d0ecc381f
CoconutDev13 <bakedbacon4@gmail.com>
2023-09-11 05:52:32 +0200

    Set default value for CLUSTERLENGTH at TurboServer constructor

--- index.test.js
+++ index.test.js
@@ -2,12 +2,12 @@ const TurboExpress = require("./lib/TurboServer");
 const RequestInterface = require("./lib/types/RequestInterface");
 const ResponseInterface = require("./lib/types/ResponseInterface");
 
-const app = new TurboExpress(1);
+const app = new TurboExpress();
 
 // 6976355584
 
-// app.get("/", (req, res) => {req.}, (req, res) => {req.});
+app.get("/", (req, res) => {res.send("Working")}, (req, res) => {req});
 
-// app.use("/asd", new TurboExpress.Router)
+app.use("/asd", new TurboExpress.Router)
 
 app.listen(3001, () => console.log("Server is running on port 3000"));
