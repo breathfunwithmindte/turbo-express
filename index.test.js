62a9446ab5b9f1f83563317ccefd57e89cdde43a
CoconutDev13 <bakedbacon4@gmail.com>
2023-09-11 06:31:42 +0200

    Implemented redirect method at ResponseInterface

--- index.test.js
+++ index.test.js
@@ -2,11 +2,24 @@ const TurboExpress = require("./lib/TurboServer");
 const RequestInterface = require("./lib/types/RequestInterface");
 const ResponseInterface = require("./lib/types/ResponseInterface");
 
+
 const app = new TurboExpress();
 
 // 6976355584
 
-app.get("/", (req, res) => {res.send("Working")}, (req, res) => {req});
+app.get("/", (req, res) => { 
+    res.redirect("/home") 
+});
+
+app.get("/home", (req, res) => { 
+    res.redirect("/about") 
+});
+
+app.get("/about", (req, res) => { 
+    res.send("Redirects test") 
+});
+
+
 
 app.use("/asd", new TurboExpress.Router)
 
